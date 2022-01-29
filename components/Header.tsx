1bdd7d51966be669ca95df755fde44a320be3514
Christian <chris.guerra120@gmail.com>
2022-01-29 13:56:47 -0700

    Add margin bottom

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import NavUserOutlet from './NavUserOutlet'
 
 const Header = () => {
   return (
-    <header className='mx-auto my-4 flex w-full max-w-4xl items-center justify-between'>
+    <header className='mx-auto my-4 mb-32 flex w-full max-w-4xl items-center justify-between'>
       <h1 className='text-2xl font-bold'>🗳️ Vota.Dev</h1>
       <div>
         <NavUserOutlet />
