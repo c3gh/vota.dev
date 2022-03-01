3128482dd55517744b8be751d19b62ec81c16c15
Christian <chris.guerra120@gmail.com>
2022-03-01 11:11:29 -0700

    Implement responsiveness

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from 'next-auth/react'
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <div className='flex min-h-screen flex-col bg-blue-600'>
+      <div className='flex min-h-screen min-w-min flex-col bg-blue-600 p-2'>
         <Component {...pageProps} />
       </div>
     </SessionProvider>
